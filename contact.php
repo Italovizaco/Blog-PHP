a2681d267bf5eab140f328b6b5a5372b21a4e283
Italo Vizaco <101332862+Italovizaco@users.noreply.github.com>
2023-03-17 12:01:58 -0300

    First commit
    Using PHP HTML and CSS to building a blog

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+include_once("templates/header.php");
+?>
+<div class="contact-container">
+  <h1>Página de contatos</h1>
+  <br>
+  <p class="contact-p">Italo R. Vizaco</p>
+  <br>
+  <a class="contact-a" href="mailto:italovizaco@gmail.com">italovizaco@gmail.com</a>
+  <br>
+  <p class="contact-p">11 97600-9533</p>
+  <br>
+  <a class="contact-a" href="https://github.com/Italovizaco">Github </a>
+  <i class="fa-brands fa-github"></i>
+  <br>
+  <a class="contact-a" href="https://www.linkedin.com/in/italovizaco/">LinkedIn </a>
+  <i class="fa-brands fa-linkedin"></i>
+</div>
+<?php
+include_once("templates/footer.php");
+?>
---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+<footer>
+  <p>Italo Vizaco &copy;2023</p>
+</footer>
+</body>
+
+</html>
